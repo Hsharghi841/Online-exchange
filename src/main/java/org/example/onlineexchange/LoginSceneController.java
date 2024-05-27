6a38cb9a68d3c860ccadc244dd8db4fbb8f054e4
Hsharghi841 <153832695+Hsharghi841@users.noreply.github.com>
2024-05-27 19:34:52 +0330

    login-view.fxml login page completed

---
+++ src/main/java/org/example/onlineexchange/LoginSceneController.java
@@ -0,0 +1,33 @@
+package org.example.onlineexchange;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class LoginSceneController implements Initializable {
+
+    @FXML
+    Button loginBtn;
+
+    @FXML
+    ImageView imageView;
+
+    @FXML
+    protected void loginBtnHandler(ActionEvent e){
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
