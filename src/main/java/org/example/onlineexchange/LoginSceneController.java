701d40d8e4458ae7b803a96d4d588b8380e58f7f
Hsharghi841 <153832695+Hsharghi841@users.noreply.github.com>
2024-06-02 22:51:10 +0330

    login page captcha finished

--- src/main/java/org/example/onlineexchange/LoginSceneController.java
+++ src/main/java/org/example/onlineexchange/LoginSceneController.java
@@ -5,12 +5,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
 
+
+
+import java.io.File;
 import java.net.URL;
-import java.util.Objects;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class LoginSceneController implements Initializable {
@@ -19,15 +22,39 @@ public class LoginSceneController implements Initializable {
     Button loginBtn;
 
     @FXML
-    ImageView imageView;
+    ImageView captchaImageView;
 
     @FXML
-    protected void loginBtnHandler(ActionEvent e){
+    TextField captchaTxf;
+
+    @FXML
+    protected void loginBtnHandler(){
+        System.out.println(captchaCheck());
 
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        reloadCaptcha();
+    }
 
+    void reloadCaptcha(){
+        Random random = new Random();
+        File file = new File("src\\main\\resources\\captcha\\samples");
+        File[] captchaFiles = file.listFiles();
+        assert captchaFiles != null;
+        String path = captchaFiles[random.nextInt(captchaFiles.length)].getAbsolutePath();
+        captchaImageView.setImage(new Image("file:/" + path));
+    }
+
+    @FXML
+    void captchaMouseClicked(){
+        reloadCaptcha();
+    }
+
+    boolean captchaCheck(){
+        String captcha = captchaImageView.getImage().getUrl();
+        captcha = captcha.substring(captcha.lastIndexOf('/') + 1, captcha.lastIndexOf('.'));
+        return captcha.equals(captchaTxf.getText());
     }
 }
