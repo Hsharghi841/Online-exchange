f45ebbfdfa9019163ff47be9901bfab3669157e1
Hsharghi841 <153832695+Hsharghi841@users.noreply.github.com>
2024-06-03 14:00:50 +0330

    captcha err added

--- src/main/java/org/example/onlineexchange/LoginSceneController.java
+++ src/main/java/org/example/onlineexchange/LoginSceneController.java
@@ -1,13 +1,15 @@
 package org.example.onlineexchange;
 
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 
 
@@ -20,22 +22,31 @@ public class LoginSceneController implements Initializable {
 
     @FXML
     Button loginBtn;
-
     @FXML
     ImageView captchaImageView;
-
     @FXML
     TextField captchaTxf;
+    @FXML
+    Label errLbl;
+    @FXML
+    VBox loginVb;
 
     @FXML
     protected void loginBtnHandler(){
-        System.out.println(captchaCheck());
+        if(!captchaCheck()){
+            printErr("captcha is incorrect");
+            return;
+        }
 
+
+        System.out.println("hi hadi");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         reloadCaptcha();
+
+        loginVb.getChildren().remove(errLbl);
     }
 
     void reloadCaptcha(){
@@ -56,5 +67,11 @@ public class LoginSceneController implements Initializable {
         String captcha = captchaImageView.getImage().getUrl();
         captcha = captcha.substring(captcha.lastIndexOf('/') + 1, captcha.lastIndexOf('.'));
         return captcha.equals(captchaTxf.getText());
+    }
+
+    void printErr(String err){
+        if(!loginVb.getChildren().contains(errLbl))loginVb.getChildren().add(loginVb.getChildren().indexOf(loginBtn), errLbl);
+        errLbl.setText(err);
+        System.err.println(err);
     }
 }
