27ed52df6bf0217f1bd8f910d898e82bd22fec5d
Hsharghi841 <153832695+Hsharghi841@users.noreply.github.com>
2024-06-03 16:48:55 +0330

    sign up added

--- src/main/java/org/example/onlineexchange/LoginSceneController.java
+++ src/main/java/org/example/onlineexchange/LoginSceneController.java
@@ -2,18 +2,22 @@ package org.example.onlineexchange;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
-
+import javafx.stage.Stage;
 
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
@@ -30,20 +34,18 @@ public class LoginSceneController implements Initializable {
     Label errLbl;
     @FXML
     VBox loginVb;
-
     @FXML
-    protected void loginBtnHandler(){
-        if(!captchaCheck()){
-            printErr("captcha is incorrect");
-            return;
-        }
+    Button signupBtn;
 
+    Stage stage;
 
-        System.out.println("hi hadi");
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+//        stage = (Stage) loginBtn.getScene().getWindow();
+
+
+
         reloadCaptcha();
 
         loginVb.getChildren().remove(errLbl);
@@ -70,8 +72,44 @@ public class LoginSceneController implements Initializable {
     }
 
     void printErr(String err){
-        if(!loginVb.getChildren().contains(errLbl))loginVb.getChildren().add(loginVb.getChildren().indexOf(loginBtn), errLbl);
+        if(!loginVb.getChildren().contains(errLbl))loginVb.getChildren().add(loginVb.getChildren().indexOf(captchaTxf) + 1, errLbl);
         errLbl.setText(err);
         System.err.println(err);
+    }
+
+    @FXML
+    protected void loginBtnHandler(){
+        if(!captchaCheck()){
+            printErr("captcha is incorrect");
+            return;
+        }
+
+
+        System.out.println("hi hadi");
+    }
+
+    @FXML
+    protected void signUpBtnHandler(){
+        if(!captchaCheck()){
+            printErr("captcha is incorrect");
+            return;
+        }
+
+
+        System.out.println("hi hadi");
+    }
+
+    @FXML
+    void signUpLblHandler(MouseEvent e) throws IOException {
+        Stage stage = (Stage) ((Label)e.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(new FXMLLoader(HelloApplication.class.getResource("signUp-view.fxml")).load(),
+                stage.getScene().getWidth(), stage.getScene().getHeight()));
+    }
+
+    @FXML
+    void loginLblHandler(MouseEvent e) throws IOException {
+        Stage stage = (Stage) ((Label)e.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(new FXMLLoader(HelloApplication.class.getResource("login-view.fxml")).load(),
+                stage.getScene().getWidth(), stage.getScene().getHeight()));
     }
 }
