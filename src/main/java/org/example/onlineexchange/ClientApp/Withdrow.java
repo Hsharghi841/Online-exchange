94e0474aa7e65c8bbcc21a055cf05e4072fd8384
Mahdiyaar <m.hosein.assarnia@gmail.com>
2024-07-13 13:46:52 +0330

    finish2

--- src/main/java/org/example/onlineexchange/ClientApp/Withdrow.java
+++ src/main/java/org/example/onlineexchange/ClientApp/Withdrow.java
@@ -9,13 +9,14 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.example.onlineexchange.Request;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class Withdrow {
 
-    boolean done;
+    boolean done,finish=true;
     double from;
     User user = User.user;
     @FXML
@@ -26,6 +27,8 @@ public class Withdrow {
     ChoiceBox<String> choiceBox2;
     @FXML
     Label value;
+    @FXML
+    Label v;
 
     public void WITHHandler(Event e) throws IOException {
         Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
@@ -38,30 +41,76 @@ public class Withdrow {
         if (Objects.equals(choiceBox1.getValue(), "USD")) {
             if (Double.valueOf(textfield.getText()) > 0 && Double.valueOf(textfield.getText()) < user.value[1]) {
                 if (Objects.equals(choiceBox2.getValue(), "USD")) {
-                    value.setText(String.valueOf((Double.valueOf(textfield.getText()) * HomepageController.coins[0].getPrice() * 0.99) / HomepageController.coins[0].getPrice()));
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[0].getPrice() * 0.99) / HomepageController.coins[0].getPrice()));
                 } else if (Objects.equals(choiceBox2.getValue(), "EUR")) {
-                    value.setText(String.valueOf((Double.valueOf(textfield.getText()) * HomepageController.coins[0].getPrice() * 0.99) / HomepageController.coins[1].getPrice()));
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[0].getPrice() * 0.99) / HomepageController.coins[1].getPrice()));
                 } else if (Objects.equals(choiceBox2.getValue(), "YEN")) {
-                    value.setText(String.valueOf((Double.valueOf(textfield.getText()) * HomepageController.coins[0].getPrice() * 0.99) / HomepageController.coins[3].getPrice()));
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[0].getPrice() * 0.99) / HomepageController.coins[3].getPrice()));
                 } else if (Objects.equals(choiceBox2.getValue(), "GBP")) {
-                    value.setText(String.valueOf((Double.valueOf(textfield.getText()) * HomepageController.coins[0].getPrice() * 0.99) / HomepageController.coins[4].getPrice()));
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[0].getPrice() * 0.99) / HomepageController.coins[4].getPrice()));
                 }
                 done = true;
             }
         } else if (Objects.equals(choiceBox1.getValue(), "EUR")) {
             if (Double.valueOf(textfield.getText()) > 0 && Double.valueOf(textfield.getText()) < user.value[2]) {
                 if (Objects.equals(choiceBox2.getValue(), "USD")) {
-                    value.setText(String.valueOf((Double.valueOf(textfield.getText()) * HomepageController.coins[1].getPrice() * 0.99) / HomepageController.coins[0].getPrice()));
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[1].getPrice() * 0.99) / HomepageController.coins[0].getPrice()));
                 } else if (Objects.equals(choiceBox2.getValue(), "EUR")) {
                     System.out.println("123");
-                    value.setText(String.valueOf((Double.valueOf(textfield.getText()) * HomepageController.coins[1].getPrice() * 0.99) / HomepageController.coins[1].getPrice()));
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[1].getPrice() * 0.99) / HomepageController.coins[1].getPrice()));
                 } else if (Objects.equals(choiceBox2.getValue(), "YEN")) {
-                    value.setText(String.valueOf((Double.valueOf(textfield.getText()) * HomepageController.coins[1].getPrice() * 0.99) / HomepageController.coins[3].getPrice()));
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[1].getPrice() * 0.99) / HomepageController.coins[3].getPrice()));
                 } else if (Objects.equals(choiceBox2.getValue(), "GBP")) {
-                    value.setText(String.valueOf((Double.valueOf(textfield.getText()) * HomepageController.coins[1].getPrice() * 0.99) / HomepageController.coins[4].getPrice()));
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[1].getPrice() * 0.99) / HomepageController.coins[4].getPrice()));
                 }
                 done = true;
             }
+        }else if (Objects.equals(choiceBox1.getValue(), "YEN")) {
+            if (Double.valueOf(textfield.getText()) > 0 && Double.valueOf(textfield.getText()) < user.value[3]) {
+                if (Objects.equals(choiceBox2.getValue(), "USD")) {
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[3].getPrice() * 0.99) / HomepageController.coins[0].getPrice()));
+                } else if (Objects.equals(choiceBox2.getValue(), "EUR")) {
+                    System.out.println("123");
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[3].getPrice() * 0.99) / HomepageController.coins[1].getPrice()));
+                } else if (Objects.equals(choiceBox2.getValue(), "YEN")) {
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[3].getPrice() * 0.99) / HomepageController.coins[3].getPrice()));
+                } else if (Objects.equals(choiceBox2.getValue(), "GBP")) {
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[3].getPrice() * 0.99) / HomepageController.coins[4].getPrice()));
+                }
+                done = true;
+            }
+        }else if (Objects.equals(choiceBox1.getValue(), "GBP")) {
+            if (Double.valueOf(textfield.getText()) > 0 && Double.valueOf(textfield.getText()) < user.value[4]) {
+                if (Objects.equals(choiceBox2.getValue(), "USD")) {
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[4].getPrice() * 0.99) / HomepageController.coins[0].getPrice()));
+                } else if (Objects.equals(choiceBox2.getValue(), "EUR")) {
+                    System.out.println("123");
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[4].getPrice() * 0.99) / HomepageController.coins[1].getPrice()));
+                } else if (Objects.equals(choiceBox2.getValue(), "YEN")) {
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[4].getPrice() * 0.99) / HomepageController.coins[3].getPrice()));
+                } else if (Objects.equals(choiceBox2.getValue(), "GBP")) {
+                    value.setText(String.valueOf(from=(Double.valueOf(textfield.getText()) * HomepageController.coins[4].getPrice() * 0.99) / HomepageController.coins[4].getPrice()));
+                }
+                done = true;
+            }
+        }
+    }
+
+    public void Done(){
+        valuefrom();
+        if(done&&finish) {
+            ClientSocket cs;
+            try {
+                cs = ClientSocket.getClientSocket();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            new Request("SWAP",textfield.getText(),choiceBox1.getValue(),String.valueOf(from),choiceBox2.getValue(),String.valueOf(Double.valueOf(textfield.getText())*0.01)).toString();
+            finish=false;
+            v.setText("معامله انجام شد به کیف پول برگردید");
+        }
+        else{
+            v.setText("امکان انجام معامله وجود ندارد");
         }
     }
 }
